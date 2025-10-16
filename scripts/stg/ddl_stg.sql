607ffe44f6a60de54d78ac305d9dfce5a929c812
erdemdlc <delicerdem@gmail.com>
2025-10-16 15:54:12 +0300

    Update ddl_stg.sql

--- scripts/stg/ddl_stg.sql
+++ scripts/stg/ddl_stg.sql
@@ -8,24 +8,43 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [stg].[offers](
-	[offer_id] [int] NULL,
+CREATE TABLE [stg].[at_teklif](
 	[submission_date] [date] NULL,
 	[offer_date] [date] NULL,
 	[candidate_full_name] [nvarchar](50) NULL,
 	[title] [nvarchar](50) NULL,
 	[customer] [nvarchar](50) NULL,
-	experience [int] NULL,
-	net_salary [int] NULL,
+	[experience] [int] NULL,
+	[net_salary] [int] NULL,
 	[starting_date] [date] NULL,
 	[recruiter] [nvarchar](50) NULL,
 	[project_leader] [nvarchar](50) NULL,
 	[source] [nvarchar](50) NULL,
 	[interview_method] [nvarchar](50) NULL,
-	[is_accepted] [bit] NULL,
 	[dwh_create_date] [datetime2](7) NULL
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [stg].[offers] ADD  DEFAULT (getdate()) FOR [dwh_create_date]
+ALTER TABLE [stg].[at_teklif] ADD  DEFAULT (getdate()) FOR [dwh_create_date]
+GO
+CREATE TABLE stg.at_aday_rapor (
+submission_date DATE
+,candidate_full_name NVARCHAR(50)
+,title NVARCHAR(50)
+,customer NVARCHAR(50)
+,experience FLOAT
+,net_salary FLOAT
+,[location] NVARCHAR(50)
+,english_level NVARCHAR(50)
+,recruiter NVARCHAR(50)
+,[source] NVARCHAR(50)
+,interview_method NVARCHAR(50)
+,elimination_reason NVARCHAR(50)
+,offer_reject_reason NVARCHAR(50)
+,linkedin_url NVARCHAR(50)
+,[dwh_create_date] [datetime2](7) 
+) 
+GO
+
+ALTER TABLE stg.at_aday_rapor ADD  DEFAULT (getdate()) FOR [dwh_create_date]
 GO

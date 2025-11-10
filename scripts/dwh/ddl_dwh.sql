1a0f96986431e50fc24ed0e77fc64aa20b2f706d
erdemdlc <delicerdem@gmail.com>
2025-11-10 15:40:25 +0300

    Update ddl_dwh.sql

--- scripts/dwh/ddl_dwh.sql
+++ scripts/dwh/ddl_dwh.sql
@@ -43,3 +43,15 @@ CREATE TABLE dwh.dim_title (
     title_key INT IDENTITY(1,1) PRIMARY KEY,
     title_name NVARCHAR(255) NOT NULL
 );
+GO
+	IF OBJECT_ID('dwh.dim_recruiter', 'U') IS NOT NULL
+    DROP TABLE dwh.dim_recruiter;
+GO
+CREATE TABLE dwh.dim_recruiter (
+    recruiter_key INT IDENTITY(1,1) PRIMARY KEY,
+    recruiter_firstname NVARCHAR(100),
+    recruiter_lastname NVARCHAR(100),
+    email NVARCHAR(255) UNIQUE,
+    phone_number NVARCHAR(50) NULL
+);
+
